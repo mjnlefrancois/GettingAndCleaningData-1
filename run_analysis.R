6f5c79db3a0fe1a1e3bde398f562c6d1535a4999
Bart Hanssens <bart.hanssens@fedict.be>
2015-07-17 19:08:37 +0200

    Corrections

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,27 @@
-library(data.table)
-library(dplyr)
+library('data.table')
+library('dplyr')
 
 subdir = 'UCI HAR Dataset'
  
 # convenience function for reading train / test file
 read.uci <- function(type, fname) {
-	f = paste('.', subdir, type, fname, sep='/')
+	f <- paste('.', subdir, type, fname, sep='/')
 	read.table(f)
 }
 
+# convenience function for merging sets
+merge.uci <- function(type = c('test', 'train')) {
+	subj <- read.uci(type, paste0('subject_', type, '.txt')) # who
+	x <- read.uci(type, paste0('X_', type, '.txt')) # big table with values
+	y <- read.uci(type, paste0('y_', type, '.txt')) # activity / what
+
+	x <- mutate(x, subj=subj$V1, activity=y$V1)	
+}
 
 # download and unzip the dataset
 if (!(file.exists(subdir))) {
-	url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-	f = 'dataset.zip'
+	url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+	f <- 'dataset.zip'
 	download.file(url, f, method='curl')
 	unzip(f)
 	unlink(f)
@@ -21,12 +29,6 @@ if (!(file.exists(subdir))) {
 
 # merge training and test sets
 
-train.subj = read.uci('train', 'subject_train.txt')
-train.x = read.uci('train', 'X_train.txt')
-train.y = read.uci('train', 'y_train.txt')
-
-test.subj = read.uci('test','subject_test.txt')
-test.x = read.uci('test', 'X_test.txt')
-test.y = read.uci('test', 'y_test.txt')
-
+train <- merge.uci('train')
+test <- merge.uci('test')
 
